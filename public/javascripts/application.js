03fbde452fb03b8e626d547b182fa930ac8851f0
Justin Emond <justin.emond@gmail.com>
2011-04-06 10:00:04 -0400

    Refine page titles. Remove legacy JS file. Refine edit form.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
-						   
 	$('#SubmitAreaControls input').attr('disabled', '');
 	
 	$(window).bind('onunload', function() { // catch if changed
